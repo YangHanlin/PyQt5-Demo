dcd25ee18031b3b849ceabf2d990ed6e9b22346e
YangHanlin <mattoncis@hotmail.com>
2019-10-20 23:03:05 +0800

    Initial commit

---
+++ pyqt_application.py
@@ -0,0 +1,40 @@
+# pyqt_application.py
+
+
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from pyqt_application_form import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.not_ready_buttons = [
+            # empty
+        ]
+        self.not_ready_actions = [
+            self.action_quit
+        ]
+        self.init_connections()
+
+    def init_connections(self):
+        for button in self.not_ready_buttons:
+            button.clicked.connect(self.work_in_progress)
+        for action in self.not_ready_actions:
+            action.triggered.connect(self.work_in_progress)
+
+    def work_in_progress(self):
+        print('This feature is not implemented; please wait.')
+
+
+def main():
+    application = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(application.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyqt_application_form.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pyqt_application_form.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.central_widget = QtWidgets.QWidget(MainWindow)
+        self.central_widget.setObjectName("central_widget")
+        MainWindow.setCentralWidget(self.central_widget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
+        self.menubar.setObjectName("menubar")
+        self.menu_file = QtWidgets.QMenu(self.menubar)
+        self.menu_file.setObjectName("menu_file")
+        MainWindow.setMenuBar(self.menubar)
+        self.action_quit = QtWidgets.QAction(MainWindow)
+        self.action_quit.setObjectName("action_quit")
+        self.menu_file.addAction(self.action_quit)
+        self.menubar.addAction(self.menu_file.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu_file.setTitle(_translate("MainWindow", "&File"))
+        self.action_quit.setText(_translate("MainWindow", "&Quit"))
