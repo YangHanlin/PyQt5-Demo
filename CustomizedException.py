373ea2046105ea1524de889d0ebb3c314533d264
YangHanlin <mattoncis@hotmail.com>
2019-10-22 22:20:50 +0800

    Advance another little

--- CustomizedException.py
+++ CustomizedException.py
@@ -1,2 +1,15 @@
+from PyQt5.QtWidgets import QMessageBox
+
 class CustomizedException(Exception):
     def __init__(self, description):
+        self.description = description
+
+    def pop_up(self, parent):
+        QMessageBox.critical(
+            parent,
+            'Error',
+            self.description
+        )
+
+    def describe_file_error(exception, path):
+        return 'Error in opening file \'{}\':\n{}'.format(path, exception.args[1])
--- pyqt_application.py
+++ pyqt_application.py
@@ -2,10 +2,12 @@
 
 
 import sys
+import os
 
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 
 from pyqt_application_form import Ui_MainWindow
+from CustomizedException import CustomizedException
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -29,6 +31,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         ]
         self.init_connections()
         self.init_chores()
+        self.reset_display_panel()
 
     def init_connections(self):
         self.action_quit.triggered.connect(self.application.quit)
@@ -41,6 +44,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def init_chores(self):
         pass
+
+    def reset_display_panel(self):
+        self.label_display_panel.setText('')
+        self.textedit_display_panel.clear()
+        self.textedit_display_panel.hide()
 
     def work_in_progress(self):
         print('This feature is not implemented yet; please wait.')
@@ -57,15 +65,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         path = self.lineedit_path.text()
         if path:
             try:
-                
+                self._open_file(path)
+            except CustomizedException as e:
+                e.pop_up(self)
+
+
+    def _open_file(self, path):
+        pattern = self.pattern_of(path)
+        if pattern == 'Text files' or pattern == 'All files':
+            try:
                 with open(path) as file:
-                    pass
+                    self.reset_display_panel()
+                    self.label_display_panel.setText(path[path.rfind('/') + len('/'):])
+                    self.textedit_display_panel.setText(file.read())
+                    self.textedit_display_panel.show()
             except FileNotFoundError as e:
-                QMessageBox.critical(
-                    self,
-                    'Error',
-                    'Error in opening file \'{}\':\n{}'.format(path, e.args[1])
-                )
+                raise CustomizedException(CustomizedException.describe_file_error(e, path))
+        elif pattern == 'Images':
+            if not os.path.exists(path):
+                raise CustomizedException(CustomizedException.describe_file_error(FileNotFoundError('No such file or directory'), path))
+            self.reset_display_panel()
+            # self.widget_display_panel.setStyleSheet('border-image: url({}); background-repeat: no-repeat; background-size: 100% 100%'.format(path))
+        else:
+            raise CustomizedException('Internal Error:\nInvalid file pattern \'{}\''.format(pattern))
+
+    def pattern_of(self, path):
+        for pattern in self.file_patterns:
+            for suffix in pattern[1]:
+                if path.endswith(suffix[suffix.rfind('*.') + len('*.'):]):
+                    return pattern[0]
 
 
 def main():
